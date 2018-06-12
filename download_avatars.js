f6ae2e1611f95c4cf65921b488cabdcefc61de95
byeong0430 <byeong.kim0430@gmail.com>
2018-06-12 01:25:51 +0000

    Added some useful comments

--- download_avatars.js
+++ download_avatars.js
@@ -1,18 +1,18 @@
 // require request and fs(file system) modules
 const request = require('request');
 const fs = require('fs');
-
 // require github token from secrets.js
 const token = require('./secrets').GITHUB_TOKEN;
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// Callback function to download image using url
 function downloadImageByURL(url, filePath){
   request.get(url)
           .on('error', function(err){
             if (err) throw err;
           })
           .on('response', function(response){
-            console.log(`Target Avatar URL: ${url} --> ${filePath}`);
             console.log('Response Status Code:', response.statusCode, response.statusMessage);
             console.log('Downloading Github Avatar...');
           })
@@ -20,22 +20,25 @@ function downloadImageByURL(url, filePath){
             fs.createWriteStream(filePath)
               .on('finish', function(){
               console.log('Download complete!');
+              console.log(`Target Avatar URL: ${url} --> ${filePath}`);
               console.log('=================================');
             })     
           );
 }
 
-// Show results or error if there is any
+// Callback function to receive a response and filter data to get user name and avatar_url
 function filterData(err, result){
   if (err) {
     console.log('Errors:', err);
   }
 
   let saveDir = './avatars/';
+  
   // Get user id and avatar_url
   let urlUserName = result.map(obj => {
     return [obj.avatar_url, `${saveDir}${obj.login}.jpg`];
   })
+  
   // downloadImageByURL(urlUserName[0][0], urlUserName[0][1]);
   urlUserName.forEach(item => downloadImageByURL(item[0], item[1]));
 }
@@ -43,6 +46,7 @@ function filterData(err, result){
 
 // cb: callback function
 function getRepoContributors(repoOwner, repoName, cb){
+  // Request options
   const options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
